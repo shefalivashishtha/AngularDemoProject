8fc7f50f46f6e0d612d10bd2cafc6317a02121ca
Shefali <shefalivashishtha@Shefalis-MacBook-Air.local>
2019-06-11 22:53:24 +0100

    first commit

---
+++ app/component.artist-item.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+@Component(
+    {
+        selector:'artist-item',
+        templateUrl:'partials/artist-item.html',
+        styleUrls:['css/artist-item.css'],
+        inputs:['artist']
+    }
+)
